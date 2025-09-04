0b066cc116a18844bce4527336a5b71e4421b687
Rajat <sharmarajat3745@gmail.com>
2025-09-04 12:24:43 +0530

    optional chaining while constructing userData

--- backend/controllers/clerkWebHooks.js
+++ backend/controllers/clerkWebHooks.js
@@ -1,4 +1,4 @@
-import {Webhook} from 'svix'
+import { Webhook } from 'svix'
 import User from '../models/User.js';
 // svix is a webhook service that clerk uses to send webhooks events securely
 
@@ -6,20 +6,20 @@ import User from '../models/User.js';
 // We are using svix library to verify signatures for secure processing of clerk webhooks, so that there are not fake webhook requests
 
 // async function - main webhook handler that will recieve webhook requests
-const clerkWebHooks = async (req,res) => {
+const clerkWebHooks = async (req, res) => {
     try {
         // creating a webhook instance by passing the secret, this secret is used to verify the request that it really comes from clerk
         // this instance is just a verifier to check the sign(coming in the request)
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
         // svix webhook req are signed , there are 3 special headers in the request i.e id, timestamp, sign, and this sign  is used to verify , we need to extract them
-        const headers={
-            'svix-id':req.headers['svix-id'],
-            'svix-timestamp':req.headers['svix-timestamp'],
-            'svix-signature':req.headers['svix-signature'],
+        const headers = {
+            'svix-id': req.headers['svix-id'],
+            'svix-timestamp': req.headers['svix-timestamp'],
+            'svix-signature': req.headers['svix-signature'],
         }
 
-        await whook.verify(JSON.stringify(req.body),headers);  
+        await whook.verify(JSON.stringify(req.body), headers);
         // what this verify will do?
         // 1. it will recreate the signed content = 'id.timestamp.body'
         // 2. put it in a algo alongwith secret to generate a sign
@@ -29,43 +29,43 @@ const clerkWebHooks = async (req,res) => {
 
 
         //Getting data from req body
-        const {data,type} = req.body;
-        
+        const { data, type } = req.body;
+
         const userData = {
-            _id:data.id,
-            email:data.email_addresses[0].email_address,
-            username:data.first_name + " "+data.last_name,
-            image:data.image_url
+            _id: data.id,
+            email: data.email_addresses?.[0]?.email_address || null,
+            username: `${data.first_name || ""} ${data.last_name || ""}`.trim(),
+            image: data.image_url || null,
 
         }
 
         // in the type we will get the events , so we will add a switch case for different events
 
-        switch(type){
-            case "user.created":{
+        switch (type) {
+            case "user.created": {
                 await User.create(userData);
-                console.log("user created ,",userData._id);
+                console.log("user created ,", userData._id);
                 break;
             }
-            case "user.updated":{
-                await User.findByIdAndUpdate(data.id,userData);
+            case "user.updated": {
+                await User.findByIdAndUpdate(data.id, userData);
                 break;
             }
-            case "user.deleted":{
+            case "user.deleted": {
                 await User.findByIdAndDelete(data.id);
                 console.log("Deleted:", data.id);
                 break;
             }
-            default: 
+            default:
                 break;
         }
 
-        res.json({success:true,message:"Webhook Recieved"})
+        res.json({ success: true, message: "Webhook Recieved" })
 
 
     } catch (error) {
         console.log(error.message)
-        res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
 
     }
 }
